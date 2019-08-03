bfa28d6acdad2d978f95e9ca80a7244419826108
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-03 13:44:25 +0400

    add: header and payments component to the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+//import { connect } from 'react-redux';
 //import * as actions from '../actions';
 
-import Header from './components/Navigation/Header.js'
-
+import Header from "./components/Navigation/Header.js";
 
 class App extends React.Component {
   componentDidMount() {
     //this.props.data
   }
 
-  render () {
+  render() {
     return (
       <div className="container">
         <BrowserRouter>
---
+++ client/src/components/Navigation/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Payments from "./Payments";
+
+class Header extends ReactComponent {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return [
+          <li key="1">
+            <Link to="/signup">Sign Up</Link>
+          </li>,
+          <li key="2">
+            <Link to="/login">Sign In</Link>
+          </li>
+        ];
+      default:
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="3" style={{ margin: "0 10px" }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="2">
+            <a href="/api/logout">Logout</a>
+          </li>
+        ];
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div>
+          <Link to={this.props.auth ? "/dashboard" : "/"}>Droom</Link>
+          <ul>{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
