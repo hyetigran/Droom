b433a2c873e79b96d62ba626be0c38c6ed923de4
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-03 12:13:14 +0400

    setup: index client file and redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import reducers from './store/reducers/reducers'
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
