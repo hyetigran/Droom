5e4117402f10ba0d3c181cae7a0f33d445f12ce1
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-03 13:46:08 +0400

    setup: actions for handling stripe payments

---
+++ client/src/store/actions/index.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { FETCH_USER } from "./types";
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post("/api/stripe", token);
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
